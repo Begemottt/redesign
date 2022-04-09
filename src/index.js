349b179242c493254e28eacea1da6488f2fefb7f
Robin VanGilder <poliosis@gmail.com>
2022-04-09 11:12:42 -0700

    Test buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import '../scss/styles.scss';
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import anime from 'animejs';
 
+// * [ Test Buttons ]
+// First, set up some event listeners
+document.getElementById('test1').addEventListener('click', ()=>testFunction1());
+document.getElementById('test2').addEventListener('click', ()=>testFunction2());
+document.getElementById('test3').addEventListener('click', ()=>testFunction3());
+
+// Then, write some functions for testing
+function testFunction1(){
+    console.log('Button 1 Pressed!!');
+}
+function testFunction2(){
+    let firstP = document.getElementsByTagName('p')[0];
+    anime({
+        targets: firstP,
+        backgroundColor: '#eee',
+        color: '#222',
+        direction: 'alternate',
+        duration: 1000,
+        easing: 'easeInQuad'
+    })
+}
+function testFunction3(){
+    console.log('Button 3 Pressed!!');
+}
