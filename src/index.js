edda69eec19f31d177ff531799c17fb5cc5b642d
Robin <poliosis@gmail.com>
2022-04-10 14:45:50 -0700

    More test button stuff

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import anime from 'animejs';
 // * [ Test Buttons ]
 // First, set up some event listeners
 document.getElementById('test1').addEventListener('click', ()=>testFunction1());
-document.getElementById('test2').addEventListener('click', ()=>testFunction2());
+document.getElementById('test2').addEventListener('click', ()=>buttonLockout('test2', 1000, testFunction2()));
 document.getElementById('test3').addEventListener('click', ()=>testFunction3());
+document.getElementById('test_activate').addEventListener('click', ()=>buttonLockout('test_activate', 500, moveButtons()));
 
 // Then, write some functions for testing
 function testFunction1(){
@@ -20,10 +21,26 @@ function testFunction2(){
         backgroundColor: '#eee',
         color: '#222',
         direction: 'alternate',
-        duration: 1000,
-        easing: 'easeInQuad'
+        duration: 500,
+        easing: 'linear'
     })
 }
 function testFunction3(){
     console.log('Button 3 Pressed!!');
+}
+
+// Activates or deactivates test button box
+function moveButtons(){
+    let el = document.getElementById('test_buttons');
+    el.classList.toggle('active');
+}
+
+// Locks out button while animation plays
+// Feed in the ID for the button, the time for the animation to play, and the function to run in the meantime!
+function buttonLockout (id, lockTime, func){
+    document.getElementById(id).disabled = true;
+    ()=>func;
+    setTimeout(() => {
+        document.getElementById(id).disabled = false;
+    }, lockTime);
 }
