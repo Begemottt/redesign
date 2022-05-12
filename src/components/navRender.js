fa8f2ef7a7c50cadbf14a8406d7171d69871c79c
Robin <poliosis@gmail.com>
2022-05-12 13:14:23 -0700

    Color palette adjustments

--- src/components/navRender.js
+++ src/components/navRender.js
@@ -4,12 +4,17 @@ import ButtonRender from './buttonRender';
 function NavRender (data) {
 
     return(
-        <nav>
+        <>
+        <nav id="top_nav">
             < ButtonRender id='top_nav_home' time={1000} func={()=>console.log('Home pressed!')} name='HOME' />
             < ButtonRender id='top_nav_about' time={1000} func={()=>console.log('About pressed!')} name='ABOUT' />
             < ButtonRender id='top_nav_portfolio' time={1000} func={()=>console.log('Portfolio pressed!')} name='PORTFOLIO' />
             < ButtonRender id='top_nav_projects' time={1000} func={()=>console.log('Projects pressed!')} name='PROJECTS' />
         </nav>
+        <nav id="side_nav">
+            
+        </nav>
+        </>
     )
 }
 
