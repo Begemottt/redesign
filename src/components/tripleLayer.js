dd3aee6b8f1e3bd708974b2de94fbf1c7818dbac
Robin <poliosis@gmail.com>
2022-06-10 14:44:35 -0700

    Fixed font loading!!

--- src/components/tripleLayer.js
+++ src/components/tripleLayer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function TripleLayer (data) {
+
     return(
         <>
             <div className={'back_layer ' + data.name}></div>
             <div className={'middle_layer ' + data.name}></div>
-            <div className={'front_layer ' + data.name}>{data.content}</div>
+            <div className={'front_layer ' + data.name}><span id={'span_' + data.name}>{data.content}</span></div>
         </>
     )
 }
